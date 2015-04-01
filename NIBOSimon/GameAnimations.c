1409c545ef864147d5fbe3c07246e769b298478b
unknown <LTI@Alphabetix.net.ingtes.ch>
2015-04-01 11:10:28 +0200

    ShowLostAnimation ausgefüllt

--- NIBOSimon/GameAnimations.c
+++ NIBOSimon/GameAnimations.c
@@ -4,10 +4,29 @@
  * Created: 01.04.2015 08:07:10
  *  Author: LTI
  */ 
+#include <nibobee/led.h>
+#include <nibobee/delay.h>
+
+void ShowLostAnimation(){
+	DrawCircle(800);
+	DrawCircle(500);
+	DrawCircle(200);		
+}
+
+void ShowEasterEggFoundAnimation(){
+	
+}
 
 
-void ShowLostAnimation()
+void ShowNextLevelAnimation(){
+	
+}
 
-void ShowEasterEggFoundAnimation()
-
-void ShowNextLevelAnimation()
+void DrawCircle(int aDelayMs){
+	for (int led = 0; led<=3; led++)
+	{
+		led_set(led,1);
+		delay(aDelayMs);
+		led_set(led,0);
+	}
+}
