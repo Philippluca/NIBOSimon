0ec6ac288ebac87f88696c59460a0d1b42911b2e
Karlo Piskor <thecombobreaker95@gmail.com>
2015-04-01 11:40:18 +0200

    Test commit

--- NIBOSimon/NIBOSimon.c
+++ NIBOSimon/NIBOSimon.c
@@ -6,13 +6,72 @@
 
 int main()
 {
-	int actualLevel = 1;
-	int sequence[1000];
-
+	led_init();//Led Initialization
+	sens_init();//Fühler initialization
+	int index = 0;//rename
+	
+	WaitForGameStartImput();
+	
+	//int actualLevel = 1;
+	//int sequence[1000];
+	
+	//while(1)//Game loop
+	//{
+		//
+	//}
 
 }
 
 int FillSequence(int aArray[1000]){
 	
 }
+int WaitForGameStartImput()
+{
+	while(1){
+		int left_Sens = sens_getLeft();
+		int right_Sens = sens_getRight();
+		if(left_Sens != 0){
+			if(left_Sens == -1)
+			{
+				led_set(LED_L_YE, 1);
+				led_set(LED_L_RD, 0);
+			}
+			if(left_Sens == 1)
+			{
+				ed_set(LED_L_YE, 0);
+				led_set(LED_L_RD, 1);
+			}
+		}
+		else
+		{
+			led_set(LED_L_YE, 0);
+			led_set(LED_L_RD, 0);
+		}
+		
+		if(right_Sens != 0)
+		{
+			if(right_Sens == -1)
+			{
+				led_set(LED_R_YE, 1);
+				led_set(LED_R_RD, 0);
+			}
+			if(right_Sens == 1)
+			{
+				led_set(LED_R_YE, 0);
+				led_set(LED_R_RD, 1);
+			}
+		}
+		else
+		{
+			led_set(LED_R_YE, 0);
+			led_set(LED_R_RD, 0);
+		}
+		break;
+	}
 	
+}
+int GetImput()
+{
+	
+	
+}
